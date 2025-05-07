6d8b821b1062ce1e034a6d83ac3b67097981180a
Luis <xluis.58x@gmail.com>
2025-05-07 07:50:14 -0500

    May 06

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,32 @@
 'use strict';
-const $form = document.getElementById('form');
 
-$form.addEventListener('submit', (event) => {
-    event.preventDefault();
-});
+ /* Crear 4 funciones que lean dos números y cada función deberá
+realizar una operación aritmetica básica + - x /
+imprimir en consola el resultado así: 10 + 5 = 15
+*/
+const num1 = 10;
+const num2 = 5;
+function suma() {
+    let resultado = num1 + num2;
+    console.log(`${num1} + ${num2} = ${resultado}`);
+}           
+
+function resta() {
+    let resultado = num1 - num2;
+    console.log(`${num1} - ${num2} = ${resultado}`);
+}
+
+function multiplicacion() {
+    let resultado = num1 * num2;
+    console.log(`${num1} * ${num2} = ${resultado}`);
+}
+
+function division() {
+    let resultado = num1 / num2;
+        console.log(`${num1} / ${num2} = ${resultado}`);
+}
+
+suma();
+resta();
+multiplicacion();
+division();
