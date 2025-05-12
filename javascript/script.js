8bb510968f54b8b9c964952ca3f1d86a0b387b0c
Luis <xluis.58x@gmail.com>
2025-05-11 21:23:12 -0500

    javacomplete

--- javascript/script.js
+++ javascript/script.js
@@ -5,9 +5,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   
-  const name = form.elements["name"].value;
-  const email = form.elements["email"].value;
-  const message = form.elements["message"].value;
+  const name = form.name.value;
+  const email = form.email.value;
+  const message = form.message.value;
   
   console.log(name);
   console.log(email);
@@ -15,4 +15,5 @@ form.addEventListener("submit", (event) => {
 
   const result = `Hi ${name}! We received your message "${message}". We'll send you an email to ${email}. We usually reply within 24h. Monday–Friday, 9AM–6PM (US time)!`; 
   alert(result);
+  form.reset();
 });
